cc463ba17c8686699afebfe2900d9c4a57c2728e
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-18 15:05:30 +0100

    Update main.h

--- main.h
+++ main.h
@@ -10,4 +10,10 @@ int _putchar(char c);
 int print_char(va_list arg);
 int print_str(va_list arg);
 int print_int(va_list arg);
+
+typedef identifierStruct
+{
+char *identifier;
+int (*function)va_list;
+}identifierStruct;
 #endif
