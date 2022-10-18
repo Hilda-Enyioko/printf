da2b43eb99f08d970ce11b534b24f079fb4a25d7
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-18 17:21:46 +0100

    Create print_identifiers.c

---
+++ print_identifiers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_identifiers - prints special characters
+ * @spc: special character after %
+ * @arg: argument
+ * Return: identifier
+ */
+int print_identifiers(char spc, va_list arg)
+{
+int i;
+identifierStruct funcs[]{
+{"c", print_char},
+{"s", print_str},
+{"d", print_int},
+{"i", print_int},
+{NULL, NULL}
+};
+for (i = 0; funcs[i].identifier != \0; i++)
+{
+if (funcs[i].identifier == spc)
+return(funcs[i].function);
+}
+return (0);
+}
