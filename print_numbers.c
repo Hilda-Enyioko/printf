201c7650e210a18e42d7944c59f7fa3a99cdac5d
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-18 14:01:51 +0100

    Create print_numbers.c

---
+++ print_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_int - prints an integer
+ * @arg: argument
+ * Return: On success 1
+ */
+int print_int(va_list arg)
+{
+int n = va_arg(arg, int);
+int digit;
+int i;
+unsigined int divisior = 1, res;
+if (n < 0)
+{
+_putchar('-');
+res++;
+n = n * -1;
+}
+
+i = 0;
+while (n / divisor > 9)
+{
+i++;
+divisor *= 10;
+}
+
