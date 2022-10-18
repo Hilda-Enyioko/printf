25639f8c5bc1b2528d75a23008b62c26c785717f
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-18 14:58:23 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printf - prints and input into the standard output
+ * _printf - produces output according to a format
  * @format: the format string
  * Return: number of bytes printed
  */
