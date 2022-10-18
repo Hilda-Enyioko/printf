2f9b051da8ff9d4f4d9bf7dd6a727df62f4b9e1e
Hilda-Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2022-10-18 17:51:44 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,57 +6,56 @@
  * Return: number of bytes printed
  */
 
-/**int _printf(const char *format, ...)
-*
-*{
-*	int sum = 0;
-*	va_list ap;
-*	char *p, *start;
-*
-*	params_t params = PARAMS_INIT;
-*
-*	va_start(ap, format);
-*
-*	if (!format || (format[0] == '%' && !format[1]))/* checking for NULL char */
-/**
-*		return (-1);
-*	if (format[0] == '%' && format[1] == ' ' && !format[2])
-*		return (-1);
-*	for (p = (char *)format; *p; p++)
-*	{
-*		init_params(&params, ap);
-*		if (*p != '%')/*checking for the % specifier*/
-*		{
-*			sum += _putchar(*p);
-*			continue;
-*		}
-*		start = p;
-*		p++;
-*		while (get_flag(p, &params)) /* while char at p is flag character */
-/**
-*		{
-*			p++; /* next character */
-*		}
-*		p = get_width(p, &params, ap);
-*		p = get_precision(p, &params, ap);
-*		if (get_modifier(p, &params))
-*			p++;
-*		if (!get_specifier(p))
-*			sum += print_from_to(start, p,
-*					params.l_modifier || params.h_modifier ? p - 1 : 0);
-*		else
-*			sum += get_print_func(p, ap, &params);
-*	}
-*	_putchar(BUF_FLUSH);
-*	va_end(ap);
-*	return (sum);
-*}
-*/
+int _printf(const char *format, ...)
+{
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+
+	params_t params = PARAMS_INIT;
+
+	va_start(ap, format);
+
+	if (!format || (format[0] == '%' && !format[1]))/* checking for NULL char */
+
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(&params, ap);
+		if (*p != '%')/*checking for the % specifier*/
+		{
+			sum += _putchar(*p);
+			continue;
+		}
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /* while char at p is flag character */
+
+		{
+			p++; /* next character */
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+					params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, ap, &params);
+	}
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
+}
 
 
-/**
-*#include "main.h"
-*/
+
+
+#include "main.h"
+/
 
 /**
  * print_char - prints character
@@ -66,21 +65,20 @@
  * Return: number chars printed
  */
 
-/**
-*int print_char(va_list ap, params_t *params)
-*{
-*	char pad_char = ' ';
-*	unsigned int pad = 1, sum = 0, ch = va_arg(ap, int);
-*
-*	if (params->minus_flag)
-*		sum += _putchar(ch);
-*	while (pad++ < params->width)
-*		sum += _putchar(pad_char);
-*	if (!params->minus_flag)
-*		sum += _putchar(ch);
-*	return (sum);
-*}
-*/
+int print_char(va_list ap, params_t *params)
+{
+	char pad_char = ' ';
+	unsigned int pad = 1, sum = 0, ch = va_arg(ap, int);
+
+	if (params->minus_flag)
+		sum += _putchar(ch);
+	while (pad++ < params->width)
+		sum += _putchar(pad_char);
+	if (!params->minus_flag)
+		sum += _putchar(ch);
+	return (sum);
+}
+
 
 /**
  * print_int - prints integer
@@ -110,41 +108,41 @@ int print_int(va_list ap, params_t *params)
  * Return: number chars printed
  */
 
-/**int print_string(va_list ap, params_t *params)
-*{
-*	char *str = va_arg(ap, char *), pad_char = ' ';
-*	unsigned int pad = 0, sum = 0, i = 0, j;
-*
-*	(void)params;
-*	switch ((int)(!str))
-*		case 1:
-*			str = NULL_STRING;
-*
-*	j = pad = _strlen(str);
-*	if (params->precision < pad)
-*		j = pad = params->precision;
-*
-*	if (params->minus_flag)
-*	{
-*		if (params->precision != UINT_MAX)
-*			for (i = 0; i < pad; i++)
-*				sum += _putchar(*str++);
-*		else
-*			sum += _puts(str);
-*	}
-*	while (j++ < params->width)
-*		sum += _putchar(pad_char);
-*	if (!params->minus_flag)
-*	{
-*		if (params->precision != UINT_MAX)
-*			for (i = 0; i < pad; i++)
-*				sum += _putchar(*str++);
-*		else
-*			sum += _puts(str);
-*	}
-*	return (sum);
-*}
-*/
+int print_string(va_list ap, params_t *params)
+{
+	char *str = va_arg(ap, char *), pad_char = ' ';
+	unsigned int pad = 0, sum = 0, i = 0, j;
+
+	(void)params;
+	switch ((int)(!str))
+		case 1:
+			str = NULL_STRING;
+
+	j = pad = _strlen(str);
+	if (params->precision < pad)
+		j = pad = params->precision;
+
+	if (params->minus_flag)
+	{
+		if (params->precision != UINT_MAX)
+			for (i = 0; i < pad; i++)
+				sum += _putchar(*str++);
+		else
+			sum += _puts(str);
+	}
+	while (j++ < params->width)
+		sum += _putchar(pad_char);
+	if (!params->minus_flag)
+	{
+		if (params->precision != UINT_MAX)
+			for (i = 0; i < pad; i++)
+				sum += _putchar(*str++);
+		else
+			sum += _puts(str);
+	}
+	return (sum);
+}
+
 
 /**
  * print_percent - prints string
