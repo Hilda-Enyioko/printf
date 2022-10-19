18001665af0f9168e5b227589b45327a2e0a57af
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-10-19 03:08:56 +0100

    strtypdef

--- main.h
+++ main.h
@@ -1,20 +1,21 @@
-#ifndef MAIN_H                                                      
-#define MAIN_H                                                      
-                                                                    
-#include <stdarg.h>                                                 
-#include <unistd.h>                                                 
-#include <stdio.h>                                                  
-                                                                    
-int _printf(const char *format, ...);                               
-int _putchar(char c);                                               
-int print_char(va_list arg);                                        
-int print_str(va_list arg);                                         
-int print_int(va_list arg);                                         
-int print_identifiers(char spc, va_list arg);                       
-                                                                    
-typedef struct identifierStruct                                     
-{                                                                   
-char *identifier;                                                   
-int (*function)(va_list);                                           
-} identifierStruct;                                                  
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_int(va_list arg);
+int print_identifiers(char spc, va_list arg);
+
+typedef struct identifierStruct
+{
+char *identifier;
+int (*function)(va_list);
+}identifierStruct;
 #endif
+
