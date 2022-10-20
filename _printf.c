e10b432ae1d8ab3bf09df2d1e770647d00acb4a4
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2022-10-20 19:10:05 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -2,48 +2,46 @@
 
 /**
  * _printf - produces output according to a format
- * @format: the format string
- * Return: number of bytes printed(res)
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-va_list arg;
-unsigned int i;
-int res = 0, identres = 0;
-va_start(arg, format);
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-if (format == NULL)
-{
-return (-1);
-}
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-_putchar(format[i]);
-res++;
-continue;
-}
-if (format[i + 1] == '%')
-{
-_putchar('%');
-res++;
-continue;
-}
-if (format[i + 1] == '\0')
-return (-1);
+	register int count = 0;
 
-identres = print_identifiers(format[i + 1], arg);
-if (identres == -1 || identres != 0)
-i++;
-if (identres > 0)
-res = res + identres;
-if (identres == 0)
-{
-_putchar('%');
-res++;
-}
-}
-va_end(arg);
-return (res);
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
---
+++ hilda_work/_printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: the format string
+ * Return: number of bytes printed(res)
+ */
+int _printf(const char *format, ...)
+{
+va_list arg;
+unsigned int i;
+int res = 0, identres = 0;
+va_start(arg, format);
+
+if (format == NULL)
+{
+return (-1);
+}
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+_putchar(format[i]);
+res++;
+continue;
+}
+if (format[i + 1] == '%')
+{
+_putchar('%');
+res++;
+continue;
+}
+if (format[i + 1] == '\0')
+return (-1);
+
+identres = print_identifiers(format[i + 1], arg);
+if (identres == -1 || identres != 0)
+i++;
+if (identres > 0)
+res = res + identres;
+if (identres == 0)
+{
+_putchar('%');
+res++;
+}
+}
+va_end(arg);
+return (res);
+}
